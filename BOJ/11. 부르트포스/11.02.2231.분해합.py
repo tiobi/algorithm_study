be73b0469338ca94aabf1756efbab629db4ac6ac
tiobi <tiobi@naver.com>
2021-12-26 10:19:05 +0900

    21.12.26

--- BOJ/11. 부르트포스/11.02.2231.분해합.py
+++ BOJ/11. 부르트포스/11.02.2231.분해합.py
@@ -1,6 +1,6 @@
 """
 # 10. 02. 2231. 분해합
-# DATE SOLVED: Solving
+# DATE SOLVED: 21.12.23
 
 # COMMENTS: 
     어떤 자연수 N의 분해합은 N과 N을 이루는 각 자리수의 합이다.
@@ -8,16 +8,25 @@
     자연수 N의 가장 작은 생성자를 구하라.
 """
 
-from sys import readline
-import numpy as np
+from sys import stdin
+
 """
 192 = 192 + 12 = 204 = 210 = 
 
 """
 
 def generator(N):
+    minGen = float('inf')
+
+    for i in range(N):
+        buf = sum(map(int, str(i)))
+        minGen = i + buf
+
+        if minGen == N:
+            return i
+
+    return 0
     pass
 
-N = int(readline())
-
+N = int(stdin.readline())
 print(generator(N))
--- BOJ/12. 정렬/12.03.u10989.수 정렬하기 3.py
+++ BOJ/12. 정렬/12.03.u10989.수 정렬하기 3.py
@@ -1,11 +1,11 @@
 """
 # 12. 03. 10989. 수 정렬하기 3
-# DATE SOLVED: Solving...
+# DATE SOLVED: 21. 11. 14
 
 # COMMENTS:
     N 개의 수를 오름차순으로 정렬.
-    O(nlogn)으로 풀어야 하는 문제. 
-    퀵 정렬로 풀어봄
+    수의 범위가 작다면 카운팅 정렬을 사용.
+     
 """
 
 import sys
