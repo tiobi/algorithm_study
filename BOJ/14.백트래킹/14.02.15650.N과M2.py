c18b777b80c69ed4a6dd8d093724c8851edb8552
tiobi <tiobi@naver.com>
2022-01-05 09:18:51 +0900

    22.01.05

--- BOJ/14.백트래킹/14.02.15650.N과M2.py
+++ BOJ/14.백트래킹/14.02.15650.N과M2.py
@@ -1,7 +1,25 @@
 """
-# 
+# 14. 02. 15650. N과 M 2
 # DATE SOLVED: Solving...
 
 # COMMENTS:
-    
+    자연수 N과 M이 주어졌을 때 중복 없이 M개를 고른 수열. 
+    오름차순.
 """
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+arr = []
+
+def DFS(array):
+    if len(array) == M:
+        print(" ".join(map(str, array)))
+
+    for i in range(1, N + 1):
+        if i not in array:
+            array.append(i)
+            DFS(array)
+            array.pop()
+
+DFS(arr)
