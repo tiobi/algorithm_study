470e9ef01b2807191976ae21f05012f2c00ac4d5
tiobi <tiobi@naver.com>
2022-01-05 20:11:01 +0900

    22.01.05

--- BOJ/14.백트래킹/14.04.15652.N과M4.py
+++ BOJ/14.백트래킹/14.04.15652.N과M4.py
@@ -1,7 +1,24 @@
 """
-# 
-# DATE SOLVED: Solving...
+# 14. 04. 15652. N과 M 4
+# DATE SOLVED: 22. 01. 05
 
 # COMMENTS:
-    
+    1부터 N까지 자연수 중 비내림차순 정렬, 같은 수 허용, M개 선택
 """
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+def DFS(array, k):
+    if len(array) == M:
+        print(" ".join(map(str, array)))
+        return
+
+    for i in range(k, N + 1):
+        array.append(i)
+        DFS(array, i)
+        array.pop()
+
+ar = []
+DFS(ar, 1)
--- BOJ/14.백트래킹/14.05.9663.N-Queen.py
+++ BOJ/14.백트래킹/14.05.9663.N-Queen.py
@@ -1,7 +1,32 @@
 """
-# 
+# 14. 05. 9663. N-Queen
 # DATE SOLVED: Solving...
 
 # COMMENTS:
-    
+    N * N 체스판 위에 퀸 N개를 서로 공격할 수 업게 놓는 문제. 
+
 """
+
+import sys
+
+N = int(sys.stdin.readline())
+
+
+def sol(arr, coor):
+    if len(arr) == N:
+        print("Y")
+        return
+
+    for i in range(coor[0], N + 1):
+        for j in range(coor[1], N + 1):
+            if i != coor[0] and j != coor[1]:
+                array.append((i,j))
+                sol(array, (i, j))
+                array.pop()
+
+
+
+array = []
+count = 0
+
+sol(array, (1, 1))
